5a3bc15a5ef10db2799e5df5790f51eeb669603f
Andy <andrewpreston1510@msn.com>
2018-03-06 22:19:36 +0000

    Add project files.

---
+++ HTML-Trim-Console/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using HTML_Trim;
+
+namespace HTML_Trim_Console
+{
+    class HTML_Trim_Console
+    {
+        static void Main(string[] args)
+        {
+            var html = "";
+
+            Console.WriteLine("Enter your HTML string");
+
+            if (!String.IsNullOrEmpty(html = Console.ReadLine()))
+            {
+                var HTML_Trim = new HTML_Trim.HTML_Trim();
+
+                Console.WriteLine("Minifying");
+                HTML_Trim.TrimHTMLString(html);
+                Console.WriteLine("HTML minified");
+            };
+
+            Console.WriteLine("Your minified HTML string is: " + Environment.NewLine + html);
+
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ HTML-Trim/HTML_Trim.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace HTML_Trim
+{
+    public class HTML_Trim
+    {
+
+        private static Regex reg = new Regex(@"(?<=[^])\t{2,}|(?<=[>])\s{2,}(?=[<])|(?<=[>])\s{2,11}(?=[<])|(?=[\n])\s{2,}");
+
+        public string TrimHTMLString(string Html)
+        {
+            if (!String.IsNullOrEmpty(Html))
+            {
+                Html = reg.Replace(Html, string.Empty);
+            }
+            else
+            {
+                return "";
+            }
+
+            return Html;
+        }
+
+    }
+}
